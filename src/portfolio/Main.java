9cf9136abbac3c9018913c89611e67fc3a8355cc
ykkpp <ykkpp@DESKTOP-PS6KIH3>
2026-01-23 07:56:51 +0900

    acpushed追加

--- src/portfolio/Main.java
+++ src/portfolio/Main.java
@@ -8,16 +8,21 @@ public class Main {
 		int num2;
 		String figure1;
 		int output;
-
-		System.out.println("好きな数字を入力してください。");
+//		boolean isACpushed = true;
+//		
+//		while(isACpushed == true)
+//		{
+//			
+		System.out.println("数字を入力");
 		num1 = new java.util.Scanner(System.in).nextInt();
+		
 		output = num1;
 
 		do {
-			System.out.println("好きな演算子（+ - × ÷）を入力してください");
+			System.out.println("演算子（+ - × ÷）を入力");
 			figure1 = new java.util.Scanner(System.in).nextLine();
 
-			System.out.println("好きな数字を入力してください。");
+			System.out.println("数字を入力");
 			num2 = new java.util.Scanner(System.in).nextInt();
 
 			if (figure1.equals("+") || figure1.equals("＋")) {
@@ -33,7 +38,7 @@ public class Main {
 				output = output / num2;
 			}
 
-			System.out.println("結果を表示したいですか？その場合はyを、処理を追加したい場合は、nを入力してください。");
+			System.out.println("結果を表示:y 処理を追加:n");
 			handan = new java.util.Scanner(System.in).nextLine();
 		} while (handan.equals("n"));
 
