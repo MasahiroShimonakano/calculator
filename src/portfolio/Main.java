560a25ac58e2803482fba93012fbf65d58d4964c
MasahiroShimonakano <masahiro17120@gmail.com>
2026-01-26 22:08:11 +0900

    ac追加

--- src/portfolio/Main.java
+++ src/portfolio/Main.java
@@ -3,47 +3,73 @@ package portfolio;
 public class Main {
 	public static void main(String[] args) {
 
+		boolean a = true;
+		boolean b = true;
 		String handan;
-		int num1;
-		int num2;
-		String figure1;
-		int output;
-//		boolean isACpushed = true;
-//		
-//		while(isACpushed == true)
-//		{
-//			
-		System.out.println("数字を入力");
-		num1 = new java.util.Scanner(System.in).nextInt();
-		
-		output = num1;
+		String enzan;
+		int output = 0;
 
-		do {
-			System.out.println("演算子（+ - × ÷）を入力");
-			figure1 = new java.util.Scanner(System.in).nextLine();
-
-			System.out.println("数字を入力");
-			num2 = new java.util.Scanner(System.in).nextInt();
-
-			if (figure1.equals("+") || figure1.equals("＋")) {
-				output = output + num2;
+		while (a) {
+			System.out.println("数字を入力 現在の数字は:" + output + "です(初期化:ac)");
+			String s1 = new java.util.Scanner(System.in).nextLine();
+			if (s1.equals("ac")) {
+				output = 0;
+				continue;
 			}
-			if (figure1.equals("-") || figure1.equals("ー")) {
-				output = output - num2;
-			}
-			if (figure1.equals("×") || figure1.equals("*")) {
-				output = output * num2;
-			}
-			if (figure1.equals("÷") || figure1.equals("/")) {
-				output = output / num2;
-			}
+			output = Integer.parseInt(s1);
 
-			System.out.println("結果を表示:y 処理を追加:n");
-			handan = new java.util.Scanner(System.in).nextLine();
-		} while (handan.equals("n"));
+			while (b) {
+				System.out.println("演算子（+ - × ÷）を入力 現在の数字は:" + output + "です (初期化:ac)");
+				enzan = new java.util.Scanner(System.in).nextLine();
+				if (enzan.equals("ac")) {
+					output = 0;
+					break;
+				}
 
-		if (handan.equals("y")) {
-			System.out.println(output);
+				System.out.println("数字を入力 現在の数字は:" + output + "です(初期化:ac)");
+				String s2 = new java.util.Scanner(System.in).nextLine();
+				if (s2.equals("ac")) {
+					output = 0;
+					break;
+				}
+				int output2 = Integer.parseInt(s2);
+
+				if (enzan.equals("+") || enzan.equals("＋")) {
+					output += output2;
+				}
+				if (enzan.equals("-") || enzan.equals("ー")) {
+					output -= output2;
+				}
+				if (enzan.equals("×") || enzan.equals("*")) {
+					output *= output2;
+				}
+				if (enzan.equals("÷") || enzan.equals("/")) {
+					output /= output2;
+				}
+
+				System.out.println("結果を表示:y 処理を追加:n 初期化;ac");
+				handan = new java.util.Scanner(System.in).nextLine();
+				if (handan.equals("ac")) {
+					output = 0;
+					break;
+				}
+
+				if (handan.equals("y")) {
+					System.out.println("結果は" + output + "でした（1"
+							+ "もう一回:enter 初期化:ac 処理を追加:n）");
+					String end = new java.util.Scanner(System.in).nextLine();
+					if (end.equals("ac")) {
+						output = 0;
+						break;
+					} else if (end.equals("n")) {
+						continue;
+					}
+					output = 0;
+					break;
+				} else if (handan.equals("n")) {
+					continue;
+				}
+			}
 		}
 	}
 }
